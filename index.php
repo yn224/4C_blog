390422eb73f5f58b969cdc4231e2f48aaf784bab
yn224 <yn224@cornell.edu>
2021-05-01 21:37:01 -0400

    [0.1.0] Initial push

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>4C Blog</title>
+  </head>
+
+  <body>
+    
+    <p>This is a paragraph</p>
+    <?php
+      // This is comment
+      echo "Hello World";
+    ?>
+
+  </body>
+</html>
